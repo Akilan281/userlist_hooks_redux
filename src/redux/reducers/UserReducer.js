9b4ff0522a1ad05babf37f8269ebdd94ffb7f5b9
Akilan281 <akilan8220@gmail.com>
2020-10-03 19:43:21 +0530

    code clean

--- src/redux/reducers/UserReducer.js
+++ src/redux/reducers/UserReducer.js
@@ -6,7 +6,7 @@ const initialstate = { userlist: [] }
  function UserReducer(state = initialstate, action) {
     switch (action.type) {
         case ADD_USER:
-            return Object.assign({}, state.userlist, { userlist: action.payload })
+            return Object.assign({}, state, { userlist: action.payload })
         default:
             return state
     }
